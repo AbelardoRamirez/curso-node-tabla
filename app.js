7b39d97536045c5ad4f1db8dba3e3325fcb57e98
Abelardo Ramirez <ja.ramirez.zambrano@gmail.com>
2021-02-04 00:45:17 -0600

    FORMAS DE MANDAR ARGUMENTAR A UNA APLICACION DE CONSOLA DE NODE.JS

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const { crearArchivo, crearArchivoAsync } = require('./helpers/multiplicar');
-const base = 4;
+//const base = 4;
 
 console.clear();
+
+console.log(process.argv);
+
+/**
+ * No se podra manejar de muchas maneras la base es decir
+ * node app --limit=20 --base=5, siempre tomaria el tercer argumento...
+ */
+
+const [, , arg3 = 5] = process.argv;
+const [,base] = arg3.split('=');
+console.log(base);
+
 crearArchivo(base)
     .then(msg => console.log(msg))
     .catch(err => console.log(err));
 
-crearArchivoAsync(10)
-    .then(msg => console.log(msg))
-    .catch(err => console.log(err));
+// crearArchivoAsync(10)
+//     .then(msg => console.log(msg))
+//     .catch(err => console.log(err));
