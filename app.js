9689a6294411f57dafc71adb4e4b94d6a07268f1
Abelardo Ramirez <ja.ramirez.zambrano@gmail.com>
2021-02-04 00:36:18 -0600

    DIFERENTES MANERAS DE MANEJAR PROMESAS & ASYNC/AWAIT.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const { crearArchivo } = require('./helpers/multiplicar');
+const { crearArchivo, crearArchivoAsync } = require('./helpers/multiplicar');
 const base = 4;
 
 console.clear();
-crearArchivo(base);
+crearArchivo(base)
+    .then(msg => console.log(msg))
+    .catch(err => console.log(err));
+
+crearArchivoAsync(10)
+    .then(msg => console.log(msg))
+    .catch(err => console.log(err));
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,6 +1,20 @@
 const fs = require('fs');
 
-const crearArchivo = (base = 5) => {
+const crearArchivo = (base = 5) => new Promise((resolve, reject) => {
+    console.clear();
+    console.log('===================');
+    console.log(`=   TABLA DEL:`, base);
+    console.log('===================');
+
+    let salida = '';
+    for (let i = 0; i < 10; i++) {
+        salida += `${base} x ${i + 1} = ${base * (i + 1)}\n`;
+    }
+    fs.writeFileSync(`tabla-${base}.txt`, salida);
+    resolve(`tabla-${base}.txt creada`);
+});
+
+const crearArchivoAsync = async (base = 5) => {
     console.clear();
     console.log('===================');
     console.log(`=   TABLA DEL:`, base);
@@ -11,12 +25,16 @@ const crearArchivo = (base = 5) => {
         salida += `${base} x ${i + 1} = ${base * (i + 1)}\n`;
     }
     console.log(salida);
-    //Aqui realiza un callBack   callBack(null,{...})
-    fs.writeFileSync(`tabla-${base}.txt`, salida);
-    console.log(`tabla-${base}.txt creada`);
-}
+    try {
+        fs.writeFileSync(`tabla-${base}.txt`, salida);
+        return `tabla-${base}.txt creada`;
+    } catch (err) {
+        throw err;
+    }
+};
 
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    crearArchivoAsync
 }
