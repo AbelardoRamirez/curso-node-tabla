aa9e896ed5b2c2e84d32aaa34420fea8a0dbb539
Abelardo Ramirez <ja.ramirez.zambrano@gmail.com>
2021-02-06 14:03:09 -0600

    LIMPIEZA DE APP.JS
    - MOVER ARCHIVOS A OTRO ARCHIVO.

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
 const { crearArchivo, crearArchivoAsync } = require('./helpers/multiplicar');
-const argv = require('yargs')
-    .option('b', {
-        alias: 'base',
-        type: 'number',
-        demandOption: true
-    })
-    .option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        demandOption: true,
-        default: false
-    })
-    .check((argv, option) => {
-        if (isNaN(argv.b)) {
-            throw 'La base tiene que ser numerico';
-        }
-        return true;
-    })
-    .argv;
+const argv = require('./config/yargs');
 
 
 crearArchivo(argv.l, argv.b)
     .then(msg => console.log(msg))
     .catch(err => console.log(err));
-
-// crearArchivoAsync(10)
-//     .then(msg => console.log(msg))
-//     .catch(err => console.log(err));
